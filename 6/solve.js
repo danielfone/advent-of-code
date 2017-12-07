306c35737c9f8e11dcf5c3ab62992ba168b6def5
Daniel Fone <daniel@fone.net.nz>
2017-12-07 15:16:38 +1300

    Day2: Crummy solve
    By the time I’ve got the answer, I’m too fatigued to refactor

--- 6/solve.js
+++ 6/solve.js
@@ -1,5 +1,5 @@
 // http://adventofcode.com/2017/day/6
-function solve (input) {
+function solve1 (input) {
   let prevIds = new Set()
   let newBank = input.slice(0)
   while (!prevIds.has(String(newBank))) {
@@ -7,6 +7,17 @@ function solve (input) {
     newBank = reallocate(newBank)
   }
   return prevIds.size
+}
+
+function solve2 (input) {
+  let prevIds = new Set()
+  let newBank = input.slice(0)
+  while (!prevIds.has(String(newBank))) {
+    prevIds.add(String(newBank))
+    newBank = reallocate(newBank)
+  }
+  const firstOccurence = [...prevIds].indexOf(String(newBank))
+  return prevIds.size - firstOccurence
 }
 
 function reallocate (banks) {
@@ -25,5 +36,7 @@ function reallocate (banks) {
 const test = [0, 2, 7, 0]
 const input = [11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11]
 
-console.log(solve(test))
-console.log(solve(input))
+console.log(solve1(test))
+console.log(solve1(input))
+console.log(solve2(test))
+console.log(solve2(input))
